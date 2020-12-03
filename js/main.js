40c1f8376e36ed3508180870159e18a08135a30c
Dmitriy Velikiy <velikiy.dmitriy91@gmail.com>
2020-12-03 03:43:27 +0200

    JS_ДЗ№1-Типы данных,вывод

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var site_message;
+site_message = "добро пожаловать на сайт записи на обсдедования МРТ,КТ";
+alert(site_message)
+
+var type_number;
+type_number = 1489;
+alert(type_number)
+
+var str = "Этот тип данных - строка"
+alert(str)
+
+var checked = true;
+checked = false;
+alert(checked)
+
+var age = null;
+alert(null)
+
+var x;
+alert(x)
+
+var clinics = ["клиника 1", "клиника 2", "клиника 3"];
+alert(clinics)
+
+var object;
+object = {cl1: "клиника 1", cl2: "клиника 2", cl3: "клиника 3"};
+alert(object)
