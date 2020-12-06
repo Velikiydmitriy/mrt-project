df08327cb1c7546dd8664d8d4e04b8137977dfd3
Dmitriy Velikiy <velikiy.dmitriy91@gmail.com>
2020-12-06 04:04:34 +0200

    JS_ДЗ№1-Типы данных,вывод,FIX #1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-var site_message;
-site_message = "добро пожаловать на сайт записи на обсдедования МРТ,КТ";
-alert(site_message)
 
+// Тип данных "Число":
 var type_number;
 type_number = 1489;
 alert(type_number)
 
+// Тип данных "Строка":
 var str = "Этот тип данных - строка"
 alert(str)
 
+// Тип данных "Булевый или Логический":
 var checked = true;
 checked = false;
 alert(checked)
 
+// Тип данных "null", "ничего" :
 var age = null;
 alert(null)
 
+// Тип данных "undefined" :
 var x;
 alert(x)
 
+// Тип данных "массив" :
 var clinics = ["клиника 1", "клиника 2", "клиника 3"];
 alert(clinics)
 
+// Тип данных "обьект", имеет ключи :
 var object;
 object = {cl1: "клиника 1", cl2: "клиника 2", cl3: "клиника 3"};
 alert(object)
