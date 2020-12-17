d0c9554400aab790a02e772923b0278007afeed0
Dmitriy Velikiy <velikiy.dmitriy91@gmail.com>
2020-12-17 22:02:36 +0200

    JS_HW2 fix4 by Dima

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Задача # 1
+/*// Задача # 1
 var question = prompt('Каково официальное название "JavaScript"', '');
 if (question === 'Ecmascript') {
     alert('Верно!');
@@ -14,25 +14,34 @@ if (number > 0) {
     alert('-1');
 } else if (number === 0) {
     alert('0');
-}
+}*/
 
 // Задача #3 ХЗ !!!!
 var question31 = prompt('Кто пришел?', 'Админ');
 if (question31 === 'Админ') {
-    alert(question32 = prompt('Пароль?', 'Черный Властелин'));
+    question32 = prompt('Пароль?', 'Черный Властелин')
+
+    if (question32 === 'Черный Властелин') {
+        alert('Добро пожаловать');
+    } else if (question32 === null) {
+        alert('Вход отменён');
+    } else {
+        alert('Пароль неверен');
+    }
 } else if (question31 === null) {
     alert('Вход отменён');
 } else {
     alert('Я Вас не знаю');
 }
-if (question32 === 'Черный Властелин') {
-    alert('Добро пожаловать');
-} else if (question32 === null) {
-    alert('Вход отменён');
-} else {
-    alert('Пароль неверен');
-}
+// if (question32 === 'Черный Властелин') {
+//     alert('Добро пожаловать');
+// } else if (question32 === null) {
+//     alert('Вход отменён');
+// } else {
+//     alert('Пароль неверен');
+// }
 
+/*
 // Задача #4
 var i = 0;
 while (i < 3) {
@@ -44,4 +53,4 @@ while (i < 3) {
 var number;
 do {
     number = prompt('Введите число больше 100');
-} while (number <= 100 && number)
+} while (number <= 100 && number)*/
